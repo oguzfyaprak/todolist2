9049252ce354aaf5872b3390dc12c5a73ff62631
Oğuz Furkan Yaprak <oguzfurkan2003@gmail.com>
2023-08-02 05:15:02 +0300

    ilk

---
+++ ToDoList Form/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ToDoList_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ekle = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Temizle = new System.Windows.Forms.Button();
+            this.Sil = new System.Windows.Forms.Button();
+            this.Değiştir = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // Ekle
+            // 
+            this.Ekle.Location = new System.Drawing.Point(311, 81);
+            this.Ekle.Name = "Ekle";
+            this.Ekle.Size = new System.Drawing.Size(75, 23);
+            this.Ekle.TabIndex = 1;
+            this.Ekle.Text = "Ekle";
+            this.Ekle.UseVisualStyleBackColor = true;
+            this.Ekle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Temizle
+            // 
+            this.Temizle.Location = new System.Drawing.Point(502, 144);
+            this.Temizle.Name = "Temizle";
+            this.Temizle.Size = new System.Drawing.Size(75, 23);
+            this.Temizle.TabIndex = 3;
+            this.Temizle.Text = "Temizle";
+            this.Temizle.UseVisualStyleBackColor = true;
+            this.Temizle.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Sil
+            // 
+            this.Sil.Location = new System.Drawing.Point(502, 185);
+            this.Sil.Name = "Sil";
+            this.Sil.Size = new System.Drawing.Size(75, 22);
+            this.Sil.TabIndex = 4;
+            this.Sil.Text = "Sil";
+            this.Sil.UseVisualStyleBackColor = true;
+            this.Sil.Click += new System.EventHandler(this.Sil_Click);
+            // 
+            // Değiştir
+            // 
+            this.Değiştir.Location = new System.Drawing.Point(502, 375);
+            this.Değiştir.Name = "Değiştir";
+            this.Değiştir.Size = new System.Drawing.Size(75, 23);
+            this.Değiştir.TabIndex = 5;
+            this.Değiştir.Text = "Değiştir";
+            this.Değiştir.UseVisualStyleBackColor = true;
+            this.Değiştir.Click += new System.EventHandler(this.Değiştir_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(502, 404);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(137, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(58, 144);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(378, 353);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listView1_ItemChecked);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(829, 535);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Değiştir);
+            this.Controls.Add(this.Sil);
+            this.Controls.Add(this.Temizle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Ekle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Ekle;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Temizle;
+        private System.Windows.Forms.Button Sil;
+        private System.Windows.Forms.Button Değiştir;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ ToDoList Form/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList_Form
+{
+    public partial class Form1 : Form
+    {
+        private List<Todo> todoList = new List<Todo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string taskText = textBox1.Text;
+
+            if (!string.IsNullOrEmpty(taskText))
+            {
+                Todo newTask = new Todo { TaskText = taskText };
+                todoList.Add(newTask);
+
+                UpdateListView();
+                
+               
+
+                textBox1.Text = "";
+            }
+            else { MessageBox.Show("Lütfen bir değer giriniz."); }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            todoList.Clear();
+        }
+
+        private void Sil_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                
+                int selectedIndex = listView1.SelectedIndices[0];
+                if (selectedIndex >= 0 && selectedIndex < todoList.Count)
+                {
+                    todoList.RemoveAt(selectedIndex);
+                    UpdateListView();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir öğe seçiniz.");
+            }
+
+        }
+
+
+        private void Değiştir_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int selectedIndex = listView1.SelectedIndices[0];
+                string newValue = textBox2.Text;
+
+                if (!string.IsNullOrEmpty(newValue))
+                {
+                    
+                    todoList[selectedIndex].TaskText = newValue;
+                    UpdateListView();
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen bir değer giriniz.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir öğe seçiniz.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Görevler", 200);
+
+        }
+
+        
+
+       
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public class Todo
+        {
+            public string TaskText { get; set; }
+            public bool Completed { get; set; }
+        }
+        private void UpdateListView()
+        {
+            listView1.Items.Clear();
+            foreach (var task in todoList)
+            {
+                string[] row = { task.TaskText };
+                var satir = new ListViewItem(row);
+                listView1.Items.Add(satir);
+                if (task.Completed) 
+                {
+                    satir.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void listView1_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int selectedIndex = listView1.SelectedIndices[0];
+                todoList[selectedIndex].Completed = !todoList[selectedIndex].Completed;
+                UpdateListView();
+            }
+        }
+    }
+}
+
